fd56efb4f0fd0176fa63d4e4f893d0d91bb5d682
Steve Womack <steve.womack@temporal.io>
2025-09-18 14:13:03 -0500

    Adds PostConstruct for debug:

--- src/main/java/com/example/TemporalSandboxApplication.java
+++ src/main/java/com/example/TemporalSandboxApplication.java
@@ -1,12 +1,25 @@
 package com.example;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TemporalSandboxApplication {
 
+    @Autowired(required = false)
+    private io.opentelemetry.api.OpenTelemetry openTelemetry;
+
     public static void main(String[] args) {
         SpringApplication.run(TemporalSandboxApplication.class, args);
+    }
+
+    @PostConstruct
+    public void checkTracing() {
+        System.out.println("OpenTelemetry bean found: " + (openTelemetry != null));
+        if (openTelemetry != null) {
+            System.out.println("OpenTelemetry class: " + openTelemetry.getClass().getName());
+        }
     }
 }
